cad9e608d0f7b52e04f5129135cddde8d166ff7f
Michael West <michael.west@alig2216.staff.aligent.consulting>
2022-05-17 16:39:48 +0930

    push

---
+++ src/public/classConstants/app/PaymentGateway/Paddle/Transaction.php
@@ -0,0 +1,23 @@
+<?php
+
+/** we define name space keyword on top of the file before any code and after declare statmenet  */
+
+declare(strict_types=1);
+
+namespace App\PaymentGateway\Paddle;
+
+#use DateTime;
+#use Notification\Email;
+
+//use VendorName\Transaction as VendorTransaction;
+
+
+class Transaction
+{
+public function __construct()
+{
+ //Adding the \ means not to load the local namespace but from global
+}
+}
+
+
